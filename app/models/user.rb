c96f522d22ea3e09db120a76a90ce9fa39149654
wahhab1404 <a.wahhab1404@gmail.com>
2019-11-24 15:28:57 +0300

    solve merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-=======
          validates :first_name, presence: true, on: :create
          validates :last_name, presence: true, on: :create
          validates :city, presence: true, on: :create
          validates :country, presence: true, on: :create
          validates :phone, presence: true, on: :create
->>>>>>> master
+
 end

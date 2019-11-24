8d72586e7759e8c3326f223bb302158cfb058868
wahhab1404 <a.wahhab1404@gmail.com>
2019-11-24 14:55:29 +0300

    test

---
+++ db/migrate/20191124094717_add_name_to_user.rb
@@ -0,0 +1,10 @@
+class AddNameToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :city, :string
+    add_column :users, :country, :string
+    add_column :users, :phone, :integer
+    add_column :users, :role, :string, :default => 'user'
+  end
+end

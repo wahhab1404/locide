36e335cc51d37713d26a6ec8575a2507372a1268
dalia <dalia.91a@gmail.com>
2019-11-25 16:52:47 +0300

    git pull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     protected
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :phone, :country, :city])
-    end 
+        devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :phone, :country, :city, :role, :about_me, :why_me])    end 
 end

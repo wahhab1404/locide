fa843169c802da2173b5c40b0fbf6df49b82fb02
wahhab1404 <a.wahhab1404@gmail.com>
2019-11-25 15:42:53 +0300

    baisc scss fot index guest page (no colours)2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create! :email => 'doe@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'John', :last_name => 'Doe', :city => 'Riyadh', :country => 'Saudi Arabia', :phone => '123456789', :role => 'guide' 
+##############
+##############
+user = User.create! :email => 'smith@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'John', :last_name => 'Smith', :city => 'Jeddah', :country => 'Saudi Arabia', :phone => '113355779', :role => 'guide'
+##############
+##############
+user = User.create! :email => 'wang@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'Frank', :last_name => 'Wong', :city => 'Riyadh', :country => 'Saudi Arabia', :phone => '112233445', :role => 'guide'
+##############
+##############
+user = User.create! :email => 'zel@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'Alicia', :last_name => 'Zelaya', :city => 'Riyadh', :country => 'Saudi Arabia', :phone => '223344556', :role => 'user'
+##############
+##############
+user = User.create! :email => 'wall@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'Jenn', :last_name => 'Wall', :city => 'Riyadh', :country => 'Saudi Arabia', :phone => '334455667', :role => 'user'
+##############
+##############
+user = User.create! :email => 'nar@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'Ramesh', :last_name => 'Narayan', :city => 'Riyadh', :country => 'Saudi Arabia', :phone => '445566778', :role => 'user'
+##############
+##############
+user = User.create! :email => 'muhannad@gmail.com', :password => '123456', :password_confirmation => '123456', :first_name => 'muhannad', :last_name => 'A', :city => 'Riyadh', :country => 'Saudi Arabia', :phone => '998866554', :role => 'user'
+##############
+##############
+##############
+##############
+##############
+##############
+##############
+##############

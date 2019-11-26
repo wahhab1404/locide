3cb9e97d4d63e00c9260ce57d964add88073a74d
Norah <no_-_no@hotmail.com>
2019-11-26 13:14:58 +0300

    getting a new version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,22 @@ class HomeController < ApplicationController
     end
   end
   def searchguest
-    @results = User.where(city: params[:q], role: 'guide')
+    @results = User.where(city: params[:q], role:'guide')
   end
   def becomeguide
     @user = current_user
   end
+  def update
+    user = User.find(params[:id])
+    user.update(guides_params)
+
+    redirect_to edit_user_registration_path
+  end
   def searchuser
     @result = Trip.where(city: params[:query])
+  end
+  private
+  def guides_params
+      params.require(:user).permit(:about_me, :why_me)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'trip/new'
   get 'home/searchuser'
   post '/trip', to: 'trip#create', as: :trips
+  post '/home', to: 'home#update', as: :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
